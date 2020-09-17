6e675f15b982030ab251500c7137103e030cb31d
michu <56609665+michutrain@users.noreply.github.com>
2020-09-16 21:45:25 -0700

    embed skeleton

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -1,4 +1,7 @@
 import discord
+import json
+import ast
+
 from discord.ext import commands
 
 class Leaderboard(commands.Cog):
@@ -11,8 +14,13 @@ class Leaderboard(commands.Cog):
         print('cog is live')
 
     @commands.command()
-    async def add(self, ctx, pt: int, name: str):
-        await ctx.send('temp')
+    async def add(self, ctx):
+        embed=discord.Embed(title="Leaderboard", description="description text")
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/755958167908909108/756009280234324078/Untitled-1.png")
+        embed.add_field(name="aaaaaaaaaaaaaaaaaaaaaaaaaaaa", value="aaaaaaaaaaaaaaaaaaaaaaaaaaaa", inline=True)
+        embed.set_footer(text="footer text")
+
+        return await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Leaderboard(bot))
